141149da13724ee8c3ff9948d841a10eb56e7042
lushuai <lushuai2010@139.com>
2016-10-08 01:31:37 +0800

    SimpleApp

---
+++ examples/src/main/scala/SimpleApp/SimpleApp.scala
@@ -0,0 +1,13 @@
+package main.scala.SimpleApp
+import org.apache.spark.SparkConf
+/**
+  * Created by lushuai on 16-10-8.
+  */
+object SimpleApp {
+
+  def main(args: Array[String]) {
+    val logFile="file:///opt/App/spark-2.0.0-bin-hadoop2.7/README.md";
+    val conf = new SparkConf()
+  }
+
+}
